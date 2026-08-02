f504e33e31299e7a3c8b72bcfbdaec6575ff61c5
Jeff Bothel <16275077+JeffBothel@users.noreply.github.com>
2026-08-01 19:58:24 -0600

    Update .gitignore and variables.tf for Terraform file exclusions and variable consistency

--- .iac/variables.tf
+++ .iac/variables.tf
@@ -2,7 +2,7 @@ variable "AZURE_SUBSCRIPTION_ID" {
   description = "The subscription ID for the Azure account."
   type        = string
   validation {
-    condition     = can(regex("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$", var.SUBSCRIPTION_ID))
-    error_message = "SUBSCRIPTION_ID must be a valid Azure subscription GUID in the format xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx."
+    condition     = can(regex("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$", var.AZURE_SUBSCRIPTION_ID))
+    error_message = "AZURE_SUBSCRIPTION_ID must be a valid Azure subscription GUID in the format xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx."
   }
 }
