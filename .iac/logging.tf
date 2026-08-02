b4f7e03017ce39029068f9d98b0426e01c31aa93
Jeff Bothel <16275077+JeffBothel@users.noreply.github.com>
2026-08-02 02:21:32 -0600

    Refactor logging configuration to implement private link scopes and scoped services for Application Insights and Log Analytics

--- .iac/logging.tf
+++ .iac/logging.tf
@@ -17,34 +17,59 @@ resource "azurerm_application_insights" "main" {
   depends_on          = [azurerm_log_analytics_workspace.main]
 }
 
-resource "azurerm_private_endpoint" "law" {
-  name                = "pep-law-csc488"
+resource "azurerm_monitor_private_link_scope" "main" {
+  name                = "ampls-csc488"
   provider            = azurerm.environment
-  location            = azurerm_resource_group.root.location
   resource_group_name = azurerm_resource_group.root.name
-  subnet_id           = azurerm_subnet.private_endpoints.id
-  depends_on          = [azurerm_log_analytics_workspace.main, azurerm_subnet.private_endpoints]
 
-  private_service_connection {
-    name                           = "psc-law-csc488"
-    private_connection_resource_id = azurerm_log_analytics_workspace.main.id
-    is_manual_connection           = false
-    subresource_names              = ["workspaces"]
-  }
+  ingestion_access_mode = "PrivateOnly"
+  query_access_mode     = "PrivateOnly"
 }
 
-resource "azurerm_private_endpoint" "appi" {
-  name                = "pep-appi-csc488"
+resource "azurerm_monitor_private_link_scoped_service" "law" {
+  name                = "ampls-law-csc488"
+  provider            = azurerm.environment
+  resource_group_name = azurerm_resource_group.root.name
+  scope_name          = azurerm_monitor_private_link_scope.main.name
+  linked_resource_id  = azurerm_log_analytics_workspace.main.id
+}
+
+resource "azurerm_monitor_private_link_scoped_service" "appi" {
+  name                = "ampls-appi-csc488"
+  provider            = azurerm.environment
+  resource_group_name = azurerm_resource_group.root.name
+  scope_name          = azurerm_monitor_private_link_scope.main.name
+  linked_resource_id  = azurerm_application_insights.main.id
+}
+
+resource "azurerm_private_endpoint" "ampls" {
+  name                = "pep-ampls-csc488"
   provider            = azurerm.environment
   location            = azurerm_resource_group.root.location
   resource_group_name = azurerm_resource_group.root.name
   subnet_id           = azurerm_subnet.private_endpoints.id
-  depends_on          = [azurerm_application_insights.main, azurerm_subnet.private_endpoints]
 
   private_service_connection {
-    name                           = "psc-appi-csc488"
-    private_connection_resource_id = azurerm_application_insights.main.id
+    name                           = "psc-ampls-csc488"
+    private_connection_resource_id = azurerm_monitor_private_link_scope.main.id
     is_manual_connection           = false
-    subresource_names              = ["components"]
+    subresource_names              = ["azuremonitor"]
   }
+
+  private_dns_zone_group {
+    name = "default"
+    private_dns_zone_ids = [
+      azurerm_private_dns_zone.zones["monitor_global"].id,
+      azurerm_private_dns_zone.zones["monitor_oms"].id,
+      azurerm_private_dns_zone.zones["monitor_ods"].id,
+      azurerm_private_dns_zone.zones["monitor_agent"].id,
+      azurerm_private_dns_zone.zones["app_insights"].id,
+    ]
+  }
+
+  depends_on = [
+    azurerm_subnet.private_endpoints,
+    azurerm_monitor_private_link_scoped_service.law,
+    azurerm_monitor_private_link_scoped_service.appi,
+  ]
 }
